739be270808a08e9be71876195adcea9e742dca9
Galaxyben <galaxyben1@gmail.com>
2018-03-15 12:52:56 -0600

    Levitación de la nave

---
+++ Assets/_LostPlanetFurroks/cameraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+	public GameObject Player;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
--- Assets/_LostPlanetFurroks/naveMovement.cs
+++ Assets/_LostPlanetFurroks/naveMovement.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class naveMovement : MonoBehaviour {
+	Rigidbody rigi;
+	public Camera Cam;
+	public GameObject piv_;
+	public float playerVel;
+	public float playerSpeed;
+	public float turnSpeed;
+	public float acceleration;
 
-	// Use this for initialization
+	public float amplitude;
+	public float frequency;
+
+	Vector3 posOffset = new Vector3 ();
+	Vector3 tempPos = new Vector3 ();
+
+	bool T_Active;
+	bool setT = false;
+	float holdTime;
+
 	void Start () {
-		
+		rigi = GetComponent <Rigidbody> ();
+		posOffset = transform.position;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+///----------------------------------------------------------------------------------------------
+
+		tempPos = posOffset;
+		tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+		transform.position = tempPos;
+
+///----------------------------------------------------------------------------------------------
+		/*Vector3 rigVel = rigi.velocity;
+		Quaternion pivrot = piv_.transform.localRotation;
+		playerVel = rigi.velocity.x;
+
+		if (Input.GetAxis ("Vertical")) {
+			holdTime += acceleration;
+			T_Active = true;
+		}*/
 	}
 }
