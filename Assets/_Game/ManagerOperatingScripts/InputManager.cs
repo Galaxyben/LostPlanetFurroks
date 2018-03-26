24c3b1c7d756c2b7ad9b4e7dad57fa3ca08ed36c
Ngakk <ngakwastaken@gmail.com>
2018-03-26 15:21:03 -0600

    Carpeta de _Game con los Mangos

---
+++ Assets/_Game/ManagerOperatingScripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos {
+	public class InputManager : MonoBehaviour {
+
+		KeyCode A = KeyCode.K, X = KeyCode.I, B = KeyCode.J, Y = KeyCode.U;
+
+		void Awake(){
+			StaticManager.inputManager = this;
+		}
+
+		void Update(){
+			switch (StaticManager.gameManager.gameState) {
+
+			case GameState.mainMenu:
+				if (Input.GetKeyDown (B)) {
+					///Tell menu to back the fuck up
+				}
+				break;
+
+			case GameState.mainGame:
+
+				break;
+
+			case GameState.pause:
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/_Game/ManagerOperatingScripts/StaticManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos {
+	public enum GameState {
+		mainMenu,
+		mainGame,
+		pause,
+	}
+
+	public enum AppState {
+		running,
+		loading
+	}
+
+	public class StaticManager {
+		public static AppManager appManager;
+		public static InputManager inputManager;
+		public static GameManager gameManager;
+	}
+}
