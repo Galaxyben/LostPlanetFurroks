b673ba90e06329b66fff091380c5f968127f2e01
Ngakk <ngakwastaken@gmail.com>
2018-03-30 18:33:57 -0600

    Audio Manager

---
+++ Assets/_Game/ManagerOperatingScripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos {	
+	public class AudioManager : MonoBehaviour {
+
+		public float volumeSFX = 1;
+		public float volumeMusic = 1;
+		public float MasterVolume = 1;
+
+		void Awake(){
+			StaticManager.audioManager = this;
+		}
+			
+	}
+}
