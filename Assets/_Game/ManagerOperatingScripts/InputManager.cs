33b803147e8cd1d57c6e13e991d09104930d2d3b
Galaxyben <galaxyben1@gmail.com>
2018-03-31 08:40:46 -0600

    BasicMovement & Shooting

--- Assets/_Game/ManagerOperatingScripts/InputManager.cs
+++ Assets/_Game/ManagerOperatingScripts/InputManager.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 namespace Mangos {
 	public class InputManager : MonoBehaviour {
-
+		/// Manager Operating Scripts
 		KeyCode A = KeyCode.K, X = KeyCode.I, B = KeyCode.J, Y = KeyCode.U;
+		float JS_h = Input.GetAxis("Horizontal");
+		float JS_v = Input.GetAxis("Vertical");
+
+		//ForceMode.
 
 		void Awake(){
 			StaticManager.inputManager = this;
@@ -21,7 +25,13 @@ namespace Mangos {
 				break;
 
 			case GameState.mainGame:
+				float velocity = 8.0f;
+				if (JS_v >= 0.05) {
+					///Si el joystick va hacia arriba
+					transform.position += (new Vector3(JS_h, 0, JS_v))*velocity*Time.deltaTime;
+				} else if (JS_v <= -0.05) {
 
+				}
 				break;
 
 			case GameState.pause:
---
+++ Assets/_Game/_PepeScripts/NaveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveScript : MonoBehaviour {
+	public GameObject Nave;
+	[Tooltip("Lugar dónde saldran los disparos")]
+	public GameObject ShootingPlace;
+	public Rigidbody Bullet;
+	private float bulletvel = 10.0f;
+	public float movementSpeed = 8.0f;
+	Rigidbody rigi;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (Input.GetAxis ("Horizontal"),Input.GetAxis ("Vertical"), 0)*movementSpeed*Time.deltaTime;
+
+		if (Input.GetButtonDown ("Fire1")) {
+			Rigidbody newBullet = Instantiate (Bullet, transform.position, Bullet.rotation) as Rigidbody;
+			newBullet.AddForce (transform.forward * bulletvel, ForceMode.VelocityChange);
+		}
+	}
+}
