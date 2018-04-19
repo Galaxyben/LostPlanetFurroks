4cbd94028ec40bb0b43b3a6f8c4ed5dde8ba75db
Ngakk <ngakwastaken@gmail.com>
2018-04-18 23:04:33 -0500

    Basic minion movement

---
+++ Assets/_Game/Minions/BasicMinionController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMinionController : MonoBehaviour {
+
+	GameObject target;
+	Rigidbody rigi;
+	Vector3 direction;
+	public float Velocidad;
+	public float TurnSpeed;
+	public float HP;
+
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody> ();
+		direction = transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			direction = (target.transform.position - transform.position) * TurnSpeed * Time.deltaTime;
+		} else {
+			direction = (transform.forward + transform.right / 50);
+		}
+		transform.LookAt (transform.position + rigi.velocity);
+		rigi.velocity = direction.normalized * Velocidad * Time.deltaTime;
+		print (rigi.velocity.magnitude);
+	}
+
+	void OnTriggerEnter(Collider _col)
+	{
+		if(_col.gameObject.CompareTag("Player"))
+		{
+			print ("entered");
+			target = _col.gameObject;
+		}
+	}
+}
---
+++ Assets/_Game/Minions/CommonGrunt/Scripts/CommonGrunt.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonGrunt : MonoBehaviour {
+
+	GameObject target;
+	public float GunDelay;
+	public float FiresPerBarrage;
+	public float BarrageDeleay;
+	float ogGD;
+	float ogBD;
+	// Use this for initialization
+	void Start () {
+		ogGD = GunDelay;
+		ogBD = BarrageDeleay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			BarrageDeleay -= Time.deltaTime;
+			if (BarrageDeleay <= 0) {
+				BarrageDeleay = ogBD;
+				ShootBarrage ();
+			}
+		}
+	}
+
+	void ShootBarrage(){
+		for (int i = 0; i < FiresPerBarrage; i++) {
+			Invoke ("Shoot", i * GunDelay);		
+		}
+	}
+
+	void Shoot(){
+
+	}
+
+	void OnTriggerEnter(Collider _col)
+	{
+		if(_col.gameObject.CompareTag("Player"))
+		{
+			print ("entered");
+			target = _col.gameObject;
+		}
+	}
+}
